04ab25fb932417049be347b654b332b236e8942b
Enerelt Bat-Erdene <enerelt2009@yahoo.co.uk>
2020-10-05 15:17:50 -0700

    Updates

---
+++ precache-manifest.5d222620eafe9cfd5625e8bdc4a4ed98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064bdbc80ba970369bdc29c76ccbdb87",
+    "url": "/Space-Robots/index.html"
+  },
+  {
+    "revision": "333b30c89d7f4ee135f5",
+    "url": "/Space-Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1affc4bff72cefca0502",
+    "url": "/Space-Robots/static/css/main.851d9700.chunk.css"
+  },
+  {
+    "revision": "333b30c89d7f4ee135f5",
+    "url": "/Space-Robots/static/js/2.37fea72a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Space-Robots/static/js/2.37fea72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1affc4bff72cefca0502",
+    "url": "/Space-Robots/static/js/main.c2d977b0.chunk.js"
+  },
+  {
+    "revision": "a1b21a1bb7518645a0c9",
+    "url": "/Space-Robots/static/js/runtime-main.9f381bd0.js"
+  },
+  {
+    "revision": "5b89e1eefefeb5142621381945d3cdc9",
+    "url": "/Space-Robots/static/media/Kallem.5b89e1ee.woff"
+  }
+]);
